4f66186733f13691595f0059688b631f031bf1a3
shilun <martin.shi@nidone.com.cn>
2017-11-13 13:44:30 +0800

    change to params

--- com.upload.web/src/main/java/com/upload/main/application/UploadApplication.java
+++ com.upload.web/src/main/java/com/upload/main/application/UploadApplication.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"com.upload", "com.common.config"})
+@ComponentScan(basePackages = {"com.upload","com.common.config"})
 public class UploadApplication {
     public static void main(String[] args) {
         SpringApplication.run(UploadApplication.class, args);
