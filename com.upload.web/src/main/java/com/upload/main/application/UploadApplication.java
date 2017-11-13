b639fdfdf1733373051d334176e57840bbf18d3f
shilun <martin.shi@nidone.com.cn>
2017-11-13 13:49:06 +0800

    change to params

--- com.upload.web/src/main/java/com/upload/main/application/UploadApplication.java
+++ com.upload.web/src/main/java/com/upload/main/application/UploadApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.upload","com.common.config"})
 public class UploadApplication {
