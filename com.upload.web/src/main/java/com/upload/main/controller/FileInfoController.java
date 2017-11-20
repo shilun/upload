5f3ede4d5cc6d502e220ae17f68714e51ff84454
shilun <martin.shi@nidone.com.cn>
2017-11-20 13:03:09 +0800

    change to params

--- com.upload.web/src/main/java/com/upload/main/controller/FileInfoController.java
+++ com.upload.web/src/main/java/com/upload/main/controller/FileInfoController.java
@@ -206,53 +206,78 @@ public class FileInfoController
 
     private void downloadExistsFile(HttpServletRequest request, HttpServletResponse response, File proposeFile) throws Exception,
             FileNotFoundException {
-        long fSize = proposeFile.length();
+        java.io.FileInputStream fis = new java.io.FileInputStream(proposeFile);
+
+        response.reset();
+
+        response.setHeader("Server", "playyuer@Microshaoft.com");
+
+        //告诉客户端允许断点续传多线程连接下载
+        //响应的格式是:
+        //Accept-Ranges: bytes
+        response.setHeader("Accept-Ranges", "bytes");
+
         long p = 0;
         long l = 0;
-        l = fSize;
+        //l = raf.length();
+        l = proposeFile.length();
+
+        //如果是第一次下,还没有断点续传,状态是默认的 200,无需显式设置
+        //响应的格式是:
+        //HTTP/1.1 200 OK
+
         if (request.getHeader("Range") != null) //客户端请求的下载的文件块的开始字节
         {
+            //如果是下载文件的范围而不是全部,向客户端声明支持并开始文件块下载
+            //要设置状态
+            //响应的格式是:
+            //HTTP/1.1 206 Partial Content
             response.setStatus(javax.servlet.http.HttpServletResponse.SC_PARTIAL_CONTENT);//206
+
+            //从请求中得到开始的字节
+            //请求的格式是:
+            //Range: bytes=[文件块的开始字节]-
             p = Long.parseLong(request.getHeader("Range").replaceAll("bytes=","").replaceAll("-",""));
         }
+
+        //下载的文件(或块)长度
+        //响应的格式是:
+        //Content-Length: [文件的总大小] - [客户端请求的下载的文件块的开始字节]
+        response.setHeader("Content-Length", new Long(l - p).toString());
+
+        if (p != 0)
+        {
+            //不是从最开始下载,
+            //响应的格式是:
+            //Content-Range: bytes [文件块的开始字节]-[文件的总大小 - 1]/[文件的总大小]
+            response.setHeader("Content-Range","bytes " + new Long(p).toString() + "-" + new Long(l -1).toString() + "/" + new Long(l).toString());
+        }
+
+        //response.setHeader("Connection", "Close"); //如果有此句话不能用 IE 直接下载
+
+        //使客户端直接下载
+        //响应的格式是:
+        //Content-Type: application/octet-stream
         response.setContentType("application/octet-stream");
-        String isoFileName = proposeFile.getName();
-        response.setHeader("Accept-Ranges", "bytes");
-        response.setHeader("Content-Length",  new Long(l - p).toString());
-        response.setHeader("Content-Disposition", "attachment; filename="
-                + isoFileName);
-        long pos = 0;
-        if (null != request.getHeader("Range")) {
-            // 断点续传
-            response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-            pos = Long.parseLong(request.getHeader("Range").replaceAll("bytes=", "").replaceAll("-", ""));
+
+        //为客户端下载指定默认的下载文件名称
+        //响应的格式是:
+        //Content-Disposition: attachment;filename="[文件名]"
+        //response.setHeader("Content-Disposition", "attachment;filename=/"" + s.substring(s.lastIndexOf("//") + 1) + "/""); //经测试 RandomAccessFile 也可以实现,有兴趣可将注释去掉,并注释掉 FileInputStream 版本的语句
+        response.setHeader("Content-Disposition", "attachment;filename=\"" + proposeFile.getName() + "\"");
+
+        //raf.seek(p);
+        fis.skip(p);
+
+        byte[] b = new byte[1024];
+        int i;
+        //while ( (i = raf.read(b)) != -1 ) //经测试 RandomAccessFile 也可以实现,有兴趣可将注释去掉,并注释掉 FileInputStream 版本的语句
+        while ( (i = fis.read(b)) != -1 )
+        {
+            response.getOutputStream().write(b,0,i);
         }
-        ServletOutputStream out = null;
-        BufferedOutputStream bufferOut = null;
-        InputStream inputStream = null;
-        try {
-            out = response.getOutputStream();
-            bufferOut = new BufferedOutputStream(out);
-            inputStream = new FileInputStream(proposeFile);
-            String contentRange = new StringBuffer("bytes ").append(
-                    new Long(pos).toString()).append("-").append(
-                    new Long(fSize - 1).toString()).append("/").append(
-                    new Long(fSize).toString()).toString();
-            response.setHeader("Content-Range", contentRange);
-            inputStream.skip(pos);
-            byte[] buffer = new byte[5 * 1024];
-            int length = 0;
-            while ((length = inputStream.read(buffer, 0, buffer.length)) != -1) {
-                bufferOut.write(buffer, 0, length);
-            }
-            bufferOut.flush();
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            IOUtils.closeQuietly(bufferOut);
-            IOUtils.closeQuietly(out);
-            IOUtils.closeQuietly(inputStream);
-        }
+        //raf.close();//经测试 RandomAccessFile 也可以实现,有兴趣可将注释去掉,并注释掉 FileInputStream 版本的语句
+        fis.close();
     }
 
     private void download(HttpServletResponse response, byte[] file, String contentType, String realName)
