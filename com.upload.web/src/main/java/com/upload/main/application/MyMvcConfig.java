db417896d6bfb55e7cef7b65a2def8cfe1f3a3d1
mac <xxx@qq.copm>
2020-07-07 18:42:40 +0800

    enable swagger

---
+++ com.upload.web/src/main/java/com/upload/main/application/MyMvcConfig.java
@@ -0,0 +1,24 @@
+package com.upload.main.application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    //所有的WebMvcConfigurerAdapter组件都会一起起作用
+    @Bean //将组件注册在容器中
+    public WebMvcConfigurer webMvcConfigurerAdapter(){
+        return new WebMvcConfigurer(){
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoginHandlerInterceptor())
+                        .addPathPatterns("*");
+            }
+        };
+    }
+
+}
