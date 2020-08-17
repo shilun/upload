668ae2e71a96691e4fa30173f3e998d38fc6644a
mac <xxx@qq.copm>
2020-08-17 23:14:57 +0800

    enable swagger

--- inotify_image.sh
+++ inotify_image.sh
@@ -1,11 +1,10 @@
 src=/data/img/
-inotifywait -mq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e create $src | while read file
+inotifywait -rmq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e create $src | while read file
 do
         file=${file/CREATE/}
         file=${file#*/}
         filetype=${file##*.}
-        if [[ "$STR" =~ .*/[0-9]{2,3}x[0-9]{2,3}/.* ]]; then
-                result=$(echo $file | grep "_")
+        if [[ $file != *_* ]];then
                 tempfilename=${file##*/}
                 name=${tempfilename%.*}
                 eval "convert -resize 50x50  /data/img/${name}/${name}.${filetype}  /data/img/${name}/${name}_50x50.${filetype}"
