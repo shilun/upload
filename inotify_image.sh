85bd21394e955fefe0e3c9cb3d876eae17be56bb
mac <xxx@qq.copm>
2020-08-18 13:36:34 +0800

    enable swagger

--- inotify_image.sh
+++ inotify_image.sh
@@ -1,17 +1,20 @@
 src=/data/img/
-inotifywait -rmq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e create $src | while read file
+inotifywait -mq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e create $src | while read file
 do
         file=${file/CREATE/}
         file=${file#*/}
+        echo "$file"
         filetype=${file##*.}
-        if [[ $file != *_*  ]];then
-                if [ -n "$filetype" ]; then
-                        tempfilename=${file##*/}
-                        name=${tempfilename%.*}
-                        eval "convert -resize 50x50  /data/img/${name}/${name}.${filetype}  /data/img/${name}/${name}_50x50.${filetype}"
-                        eval "convert -resize 100x100  /data/img/${name}/${name}.${filetype}  /data/img/${name}/${name}_100x100.${filetype}"
-                        eval "convert -resize 400x400  /data/img/${name}/${name}.${filetype}  /data/img/${name}/${name}_400x400.${filetype}"
-                        echo "STRING is not empty"
-                fi
+        if [[ $file == *ISDIR ]]; then
+                echo "dir"
+        else
+                echo "$file"
+                tempfilename=${file##*/}
+                name=${tempfilename%.*}
+                eval "mkdir /data/img/${name} "
+                echo "convert -resize 50x50  /data/img/${name}.${filetype}  /data/img/${name}/${name}_50x50.${filetype}"
+                eval "convert -resize 50x50  /data/img/${name}.${filetype}  /data/img/${name}/${name}_50x50.${filetype}"
+                eval "convert -resize 100x100  /data/img/${name}.${filetype}  /data/img/${name}/${name}_100x100.${filetype}"
+                eval "convert -resize 400x400  /data/img/${name}.${filetype}  /data/img/${name}/${name}_400x400.${filetype}"
         fi
 done
